9e4facd25b62884002dcacc2605c869704204b91
rileyfroehlich <49214686+rileyfroehlich@users.noreply.github.com>
2021-01-28 11:00:44 -0800

    Done

--- app.js
+++ app.js
@@ -10,10 +10,13 @@ var handlebars = require('express3-handlebars')
 
 var index = require('./routes/index');
 var hello = require('./routes/hello');
+var project = require("./routes/project")
 // Example route
-// var user = require('./routes/user');
+
 
 var app = express();
+var hello = require('./routes/hello');
+app.get('hello/:userName', hello.view)
 
 // all environments
 app.set('port', process.env.PORT || 3000);
@@ -38,6 +41,7 @@ if ('development' == app.get('env')) {
 // Add routes here
 app.get('/', index.view);
 app.get('/hello/:userName', hello.view);
+app.get("/project/:name", project.viewProject)
 // Example route
 // app.get('/users', user.list);
 
